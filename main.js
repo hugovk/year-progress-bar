51cdb13d6a1b67434fbd1b68c3fb046da8759e14
hugovk <hugovk@users.noreply.github.com>
2016-01-02 21:14:34 +0200

    Update progress bar every second

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ window.onload = function onLoad() {
     color: '#FCB03C'
   });
 
-  var now = new Date();
-  var start = new Date(now.getFullYear(), 0, 0);
-  var end = new Date(now.getFullYear() + 1, 0, 0);
-  var done = (now-start) / (end-start);
+  function progress() {
+    var now = new Date();
+    var start = new Date(now.getFullYear(), 0, 0);  // Start of this year
+    var end = new Date(now.getFullYear() + 1, 0, 0);  // End of this year
+    var done = (now-start) / (end-start);
+    return done;
+  }
 
-  line.animate(done);  // Number from 0.0 to 1.0
+  line.animate(progress());  // Number from 0.0 to 1.0
+
+  setInterval(update, 1000);
+
+  function update() {
+    line.set(progress());
+  }
+
 };
